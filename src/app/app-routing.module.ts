9374862df4e61c3be20fc17a03acd4ebefb13302
Jahanzaib khan <jahanzaibkhan9999@gmail.com>
2024-06-09 19:42:39 +0500

    some modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -108,7 +108,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [],
 })
